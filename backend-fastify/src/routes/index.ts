91f513e97ec7654e8d86f7014ad60ddf26402d4e
PedroSmaxY <pedrohnovais@outlook.com>
2025-04-25 23:17:09 -0300

    atualizando o registro das rotas para incluir prefixos e simplificar a estrutura do código

--- backend-fastify/src/routes/index.ts
+++ backend-fastify/src/routes/index.ts
@@ -1,6 +1,12 @@
 import { FastifyInstance } from "fastify";
 import { registerPlayerRoutes } from "./player-routes.js";
+import registerTeamRoutes from "./team-routes.js";
 
 export async function registerRoutes(app: FastifyInstance) {
-  await registerPlayerRoutes(app);
+  await app.register(registerPlayerRoutes, { prefix: "/players" });
+  await app.register(registerTeamRoutes, { prefix: "/furia" });
+
+  app.get("/", async (request, reply) => {
+    return reply.send({ message: "Welcome to the HLTV API" });
+  });
 }
