86234ee14426b60cde2e2e87a2c175bfd95116f3
PedroSmaxY <pedrohnovais@outlook.com>
2025-04-26 04:55:16 -0300

    refatorando rota de jogador para suportar identificadores numéricos e por nome

--- backend-fastify/src/routes/players-routes.ts
+++ backend-fastify/src/routes/players-routes.ts
@@ -1,31 +1,56 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
-import { HLTV } from "hltv";
+import { FullPlayer, HLTV } from "hltv";
+
+type PlayerParams = {
+  identifier: string;
+};
 
 export async function registerPlayerRoutes(app: FastifyInstance) {
-  app.get("/", async (_, reply: FastifyReply) => {
-    try {
-      const players = await HLTV.getPlayerRanking();
-      return reply.send(players);
-    } catch (error) {
-      app.log.error(error);
-      return reply
-        .status(500)
-        .send({ error: "Falha ao buscar ranking de jogadores" });
-    }
-  });
-
   app.get(
-    "/:id",
+    "/:identifier",
     async (
-      request: FastifyRequest<{
-        Params: { id: string };
-      }>,
+      request: FastifyRequest<{ Params: PlayerParams }>,
       reply: FastifyReply
     ) => {
       try {
-        const playerId = parseInt(request.params.id);
-        const player = await HLTV.getPlayer({ id: playerId });
-        return reply.send(player);
+        const identifier = request.params.identifier;
+
+        const isNumeric = /^\d+$/.test(identifier);
+
+        let player: FullPlayer | null = null;
+        if (isNumeric) {
+          const playerId = parseInt(identifier);
+          player = await HLTV.getPlayer({ id: playerId });
+        } else {
+          player = await HLTV.getPlayerByName({ name: identifier });
+
+          if (!player) {
+            return reply.status(404).send({ error: "Jogador não encontrado" });
+          }
+        }
+
+        return reply.send({
+          player: {
+            playerId: player.id,
+            name: player.name,
+            image: player.image,
+            age: player.age,
+            rating: player.statistics?.rating,
+            team: player.team,
+            country: player.country,
+          },
+          socials: {
+            instagram: player.instagram,
+            twitter: player.twitter,
+            twitch: player.twitch,
+          },
+          news: player.news.slice(0, 5).map((news) => {
+            return {
+              title: news.name,
+              link: `https://www.hltv.org${news.link}`,
+            };
+          }),
+        });
       } catch (error) {
         app.log.error(error);
         return reply
