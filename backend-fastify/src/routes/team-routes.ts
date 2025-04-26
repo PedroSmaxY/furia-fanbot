095a742be468ba72b510604a9a21e76470756b11
PedroSmaxY <pedrohnovais@outlook.com>
2025-04-26 04:29:01 -0300

    adicionando rota de resumo para informações detalhadas da equipe

--- backend-fastify/src/routes/team-routes.ts
+++ backend-fastify/src/routes/team-routes.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { FullTeam, FullTeamPlayer, HLTV } from "hltv";
+import { FullTeamStats } from "hltv/lib/endpoints/getTeamStats.js";
 
 const TEAM_ID = 8297;
 
@@ -73,6 +74,53 @@ export async function registerTeamRoutes(app: FastifyInstance) {
       });
 
       return reply.send({ total: news.length, news: news });
+    } catch (error) {
+      app.log.error(error);
+      return reply.status(500).send({ error: "Failed to fetch team matches" });
+    }
+  });
+
+  app.get("/summary", async (_, reply: FastifyReply) => {
+    try {
+      const teamStats: FullTeamStats = await HLTV.getTeamStats({
+        id: TEAM_ID,
+      });
+      const teamInfos: FullTeam = await HLTV.getTeam({ id: TEAM_ID });
+
+      const summary = {
+        info: {
+          id: teamInfos.id,
+          name: teamInfos.name,
+          logo: teamInfos.logo,
+          instagram: teamInfos.instagram,
+          country: teamInfos.country,
+        },
+        ranking: {
+          current: teamInfos.rank,
+          history: teamInfos.rankingDevelopment.slice(0, 5),
+        },
+        roster: [
+          ...teamInfos.players
+            .filter((player) => player.type !== "Coach")
+            .map((player) => ({
+              name: player.name,
+              type: player.type,
+            })),
+        ],
+        coach: {
+          name: teamInfos.players.find((player) => player.type === "Coach")
+            ?.name,
+        },
+        stats: teamStats.overview,
+        maps: Object.entries(teamStats.mapStats).map(([mapName, stats]) => ({
+          name: mapName.replace("de_", " ").trim(),
+          played: stats.wins + stats.draws + stats.losses,
+          winRate: stats.winRate,
+        })),
+        achievements: teamStats.events.filter((event) => event.place == "1st"),
+      };
+
+      return reply.send({ summary: summary });
     } catch (error) {
       app.log.error(error);
       return reply.status(500).send({ error: "Failed to fetch team matches" });
