439e116e693a9c7c8f6d54636d4039a850a58b09
PedroSmaxY <pedrohnovais@outlook.com>
2025-04-26 20:33:02 -0300

    alterando a função get_summary para ser síncrona e retornando um objeto Summary a partir dos dados da API

--- bot/src/services/api_client.py
+++ bot/src/services/api_client.py
@@ -1,25 +1,20 @@
 import httpx
 import os
 from dotenv import load_dotenv
+from src.models.summary_model import Summary
 
 load_dotenv()
 API_URL = os.getenv("API_URL")
 
 
-async def get_summary():
-    async with httpx.AsyncClient() as client:
-        response = await client.get(f"{API_URL}/team/summary")
-        response.raise_for_status()
-        return response.json()
+def get_summary() -> Summary:
+    client = httpx.Client()
+    response = client.get(f"{API_URL}/team/summary")
+    response.raise_for_status()
+    data = response.json()['summary']
+    return Summary.from_dict(data)
 
 
 if __name__ == "__main__":
-    import asyncio
-
-
-    async def main():
-        summary = await get_summary()
-        print(summary)
-
-
-    asyncio.run(main())
+    summary = get_summary()
+    print(summary.info)
