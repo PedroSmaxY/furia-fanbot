a33da2d480fb209a28d35ab7f23edbece477ed2b
PedroSmaxY <pedrohnovais@outlook.com>
2025-04-26 18:44:07 -0300

    adicionando funcionalidade básica ao ChatBot com telebot

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,21 @@
 from dotenv import load_dotenv
+import telebot
 from os import getenv
 
 load_dotenv()
 
-print(getenv('API_KEY'))
+API_KEY = getenv("API_KEY")
+
+bot = telebot.TeleBot(API_KEY)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id,
+                     "Hello! I'm a bot that can help you with various tasks. Type /help to see what I can do.")
+    print(f"User {message.from_user.username} started the bot.")
+
+
+print("Bot is running...")
+bot.infinity_polling()
+print("Bot has stopped.")
