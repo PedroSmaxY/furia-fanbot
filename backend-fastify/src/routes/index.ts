70139ea15c9e3c8a165dfcb647f13537272612cb
PedroSmaxY <pedrohnovais@outlook.com>
2025-04-26 00:30:14 -0300

    atualizando as rotas da API para corrigir o nome do arquivo de jogadores e reorganizar os prefixos das rotas

--- backend-fastify/src/routes/index.ts
+++ backend-fastify/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { FastifyInstance } from "fastify";
-import { registerPlayerRoutes } from "./player-routes.js";
+import { registerPlayerRoutes } from "./players-routes.js";
 import registerTeamRoutes from "./team-routes.js";
+import registerStatsRoutes from "./stats-routes.js";
 
 export async function registerRoutes(app: FastifyInstance) {
   await app.register(registerPlayerRoutes, { prefix: "/players" });
-  await app.register(registerTeamRoutes, { prefix: "/furia" });
+  await app.register(registerStatsRoutes, { prefix: "/stats" });
+  await app.register(registerTeamRoutes, { prefix: "/team" });
 
   app.get("/", async (request, reply) => {
     return reply.send({ message: "Welcome to the HLTV API" });
