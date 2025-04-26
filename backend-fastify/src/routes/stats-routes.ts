a9a66b3faadbbe9f4db1e24ce98a3a4512d00d44
PedroSmaxY <pedrohnovais@outlook.com>
2025-04-26 00:30:21 -0300

    adicionando rotas para obter estatísticas da equipe

---
+++ backend-fastify/src/routes/stats-routes.ts
@@ -0,0 +1,60 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { HLTV } from "hltv";
+import { FullTeamStats } from "hltv/lib/endpoints/getTeamStats.js";
+
+const TEAM_ID = 8297;
+
+export default async function registerStatsRoutes(app: FastifyInstance) {
+  app.get("/", async (_, reply) => {
+    try {
+      const teamStats: FullTeamStats = await HLTV.getTeamStats({
+        id: TEAM_ID,
+      });
+
+      return reply.send({ stats: teamStats });
+    } catch (error) {
+      app.log.error(error);
+      return reply.status(500).send({ error: "Failed to fetch team matches" });
+    }
+  });
+
+  app.get("/overview", async (_, reply) => {
+    try {
+      const teamStats: FullTeamStats = await HLTV.getTeamStats({
+        id: TEAM_ID,
+      });
+
+      return reply.send({ overview: teamStats.overview });
+    } catch (error) {
+      app.log.error(error);
+      return reply.status(500).send({ error: "Failed to fetch team matches" });
+    }
+  });
+
+  app.get("/matches", async (request: FastifyRequest, reply: FastifyReply) => {
+    const limit = Number((request.query as any).limit) || 5;
+
+    try {
+      const teamStats: FullTeamStats = await HLTV.getTeamStats({
+        id: TEAM_ID,
+      });
+      const recent = teamStats.matches.slice(0, limit);
+
+      const formattedMatches = recent.map((match) => ({
+        date: new Date(match.date).toLocaleDateString("pt-BR"),
+        opponent: match.team2.name,
+        map: match.map.replace("de_", " ").trim(),
+        score: `${match.result.team1}-${match.result.team2}`,
+        eventName: match.event.name,
+      }));
+
+      return reply.send({
+        total: formattedMatches.length,
+        matches: formattedMatches,
+      });
+    } catch (error) {
+      app.log.error(error);
+      return reply.status(500).send({ error: "Failed to fetch team matches" });
+    }
+  });
+}
