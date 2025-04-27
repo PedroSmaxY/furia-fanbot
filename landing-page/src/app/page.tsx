26de8606e4ff132a71904416478e953f408952e7
PedroSmaxY <pedrohnovais@outlook.com>
2025-04-27 15:09:07 -0300

    Adicionando a landing page do projeto

---
+++ landing-page/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <h1 className="text-2xl font-bold">SeX</h1>
+        <Button variant="default" className="">
+          Botão
+        </Button>
+      </main>
+    </>
+  );
+}
