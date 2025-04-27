f8e1bdb979fa7d1e9bad3a2e096effb011f13be8
PedroSmaxY <pedrohnovais@outlook.com>
2025-04-26 21:20:21 -0300

    atualizando manipulador de jogadores para utilizar a função get_roster e exibir a lista dinâmica de jogadores da FURIA

--- bot/src/handlers/players.py
+++ bot/src/handlers/players.py
@@ -1,14 +1,18 @@
 import telebot
 from telebot import TeleBot
 
+from src.services.api_client import get_roster
+
 
 def players_handler(bot: TeleBot):
     @bot.message_handler(commands=['players'])
     def handle_players(message: telebot.types.Message):
+        players = get_roster().players
+
         bot.send_message(message.chat.id,
-                         "👥 Elenco da FURIA:\n"
-                         "1. 🧑‍🦰 arT\n"
-                         "2. 👨‍🦱 drop\n"
-                         "3. 👨‍🦲 saffee\n"
-                         "4. 👨‍🦳 yuurih\n"
-                         "5. 👨‍🦰 jks\n")
+                         f"👥 Elenco da FURIA:\n"
+                         f"1. {players[0].name}\n"
+                         f"2. {players[1].name}\n"
+                         f"3. {players[2].name}\n"
+                         f"4. {players[3].name}\n"
+                         f"5. {players[4].name}\n")
