4a124ce22fa72ef3f6c0b97d142db74d9299c0a4
PedroSmaxY <pedrohnovais@outlook.com>
2025-04-26 23:55:09 -0300

    adicionando logo da equipe na mensagem de boas-vindas do bot

--- bot/src/handlers/start.py
+++ bot/src/handlers/start.py
@@ -5,5 +5,7 @@ from telebot import TeleBot
 def start_handler(bot: TeleBot):
     @bot.message_handler(commands=['start'])
     def handle_start(message: telebot.types.Message):
-        bot.send_message(message.chat.id,
-                         "👋 Olá! Eu sou o Bot da FURIA.\nUse /resumo, /proximos, /resultados, /elenco e muito mais!")
+        team_logo = "https://apiesltv.imgix.net/images/team/logo/180_6389fd40-d1b3-4bd3-9a64-6ede7e24bd38.png?auto=compress&w=400"
+
+        bot.send_photo(message.chat.id, team_logo,
+                       "👋 Olá! Eu sou o Bot da FURIA.\nUse /resumo, /proximos, /resultados, /elenco e muito mais!")
