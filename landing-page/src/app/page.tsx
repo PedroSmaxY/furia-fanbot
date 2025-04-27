5b166f70371511fe808c72830a274a00c5d40bcd
PedroSmaxY <pedrohnovais@outlook.com>
2025-04-27 16:19:43 -0300

    adicionando logo da Furia e removendo botão com ícone do WhatsApp da página inicial

--- landing-page/src/app/page.tsx
+++ landing-page/src/app/page.tsx
@@ -1,15 +1,10 @@
-import { Button } from "@/components/ui/button";
 import { Hero } from "./_components/hero";
-import { WhatsappLogo } from "@phosphor-icons/react/dist/ssr";
 
 export default function Home() {
   return (
     <>
       <main>
         <Hero />
-        <Button variant="default" className="">
-          Botão <WhatsappLogo />
-        </Button>
       </main>
     </>
   );
