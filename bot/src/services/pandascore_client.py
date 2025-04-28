04cdc4818d0a50696bf1f4c3e12de2e20b888868
PedroSmaxY <pedrohnovais@outlook.com>
2025-04-28 19:07:53 -0300

    adicionando cache para otimização de requisições à API do Pandascore

--- bot/src/services/pandascore_client.py
+++ bot/src/services/pandascore_client.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from os import getenv
 import httpx
+import time
 from src.models.pandascore_models import Team, PlayerDetail, MatchList
 
 load_dotenv()
@@ -8,8 +9,18 @@ load_dotenv()
 PANDASCORE_API_KEY = getenv('PANDASCORE_API_KEY')
 BASE_URL = 'https://api.pandascore.co'
 
+_cache = {}
+DEFAULT_CACHE_EXPIRY = 30 * 60
 
-def get_pandascore_data(endpoint, params=None):
+
+def get_pandascore_data(endpoint, params=None, cache_ttl=DEFAULT_CACHE_EXPIRY):
+    cache_key = f"{endpoint}:{str(params)}"
+
+    current_time = time.time()
+    if cache_key in _cache and _cache[cache_key][1] > current_time:
+        print(f"Usando dados em cache para {endpoint}")
+        return _cache[cache_key][0]
+
     headers = {
         'accept': 'application/json',
         'authorization': 'Bearer ' + PANDASCORE_API_KEY
@@ -17,23 +28,26 @@ def get_pandascore_data(endpoint, params=None):
 
     response = httpx.get(f"{BASE_URL}/{endpoint}", headers=headers, params=params)
 
-    print(response.url)
+    print(f"Requisição à API para {response.url}")
 
     response.raise_for_status()
 
     if response.status_code == 200:
-        return response.json()
+        data = response.json()
+        expiry_time = current_time + cache_ttl
+        _cache[cache_key] = (data, expiry_time)
+        return data
     else:
         print(f"Erro: {response.status_code} - {response.text}")
         return None
 
 
-def get_team() -> Team:
-    team = get_pandascore_data(endpoint="/teams/furia")
+def get_team(cache_ttl=DEFAULT_CACHE_EXPIRY) -> Team:
+    team = get_pandascore_data(endpoint="/teams/furia", cache_ttl=cache_ttl)
     return Team.from_dict(team)
 
 
-def get_future_matches(team_id_or_slug="furia") -> MatchList:
+def get_future_matches(team_id_or_slug="furia", cache_ttl=DEFAULT_CACHE_EXPIRY) -> MatchList:
     params = {
         'filter[future]': True,
         'sort': 'begin_at',
@@ -42,15 +56,22 @@ def get_future_matches(team_id_or_slug="furia") -> MatchList:
 
     matches = get_pandascore_data(
         endpoint=f"/teams/{team_id_or_slug}/matches",
-        params=params
+        params=params,
+        cache_ttl=cache_ttl
     )
 
     return MatchList.from_dict(matches)
 
 
-def get_player(id_or_slug: str) -> PlayerDetail:
-    player = get_pandascore_data(endpoint=f"/players/{id_or_slug}")
+def get_player(id_or_slug: str, cache_ttl=DEFAULT_CACHE_EXPIRY) -> PlayerDetail:
+    player = get_pandascore_data(endpoint=f"/players/{id_or_slug}", cache_ttl=cache_ttl)
     return PlayerDetail.from_dict(player)
+
+
+def clear_cache():
+    global _cache
+    _cache = {}
+    print("Cache limpo com sucesso")
 
 
 if __name__ == "__main__":
