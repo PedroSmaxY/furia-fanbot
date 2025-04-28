c8136f88473f9870db28c59f7185c954dc39ebe2
PedroSmaxY <pedrohnovais@outlook.com>
2025-04-28 14:44:17 -0300

    adicionando função para obter dados da equipe FURIA da API da Pandascore

--- bot/src/services/pandascore_client.py
+++ bot/src/services/pandascore_client.py
@@ -2,25 +2,36 @@ from dotenv import load_dotenv
 from os import getenv
 import httpx
 
+from src.models.pandascore_models import Team
+
 load_dotenv()
 
 PANDASCORE_API_KEY = getenv('PANDASCORE_API_KEY')
-
-headers = {
-    'accept': 'application/json',
-    'authorization': 'Bearer ' + PANDASCORE_API_KEY
-}
+BASE_URL = 'https://api.pandascore.co'
 
 
-def get_matches():
-    url = 'https://api.pandascore.co/matches?sort=-modified_at'
-    client = httpx.Client()
-    client.headers.update(headers)
-    response = client.get(url)
+def get_pandascore_data(endpoint, params=None):
+    headers = {
+        'accept': 'application/json',
+        'authorization': 'Bearer ' + PANDASCORE_API_KEY
+    }
+
+    response = httpx.get(f"{BASE_URL}/{endpoint}", headers=headers, params=params)
+
     response.raise_for_status()
-    data = response.json()
-    return data
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Erro: {response.status_code} - {response.text}")
+        return None
+
+
+def get_team() -> Team:
+    team = get_pandascore_data(endpoint="/teams/furia")
+
+    return Team.from_dict(team)
 
 
 if __name__ == "__main__":
-    print(get_matches())
+    print(get_team())
