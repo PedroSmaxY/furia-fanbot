5e297eff4fe06c4f16d4520cc52723c67e5e2ae0
PedroSmaxY <pedrohnovais@outlook.com>
2025-04-29 01:15:57 -0300

    Atualiza variáveis de ambiente para usar API_BASE_URL e ajusta chamadas de API no cliente

--- bot/src/services/api_client.py
+++ bot/src/services/api_client.py
@@ -4,33 +4,33 @@ from dotenv import load_dotenv
 from src.models.models import Summary, Roster, MatchesResponse, NewsResponse
 
 load_dotenv()
-API_URL = os.getenv("API_URL")
+API_BASE_URL = os.getenv("API_BASE_URL", "http://localhost:3000/api/v1/furia")
 
 
 def get_api_data(endpoint: str) -> dict:
     client = httpx.Client()
-    response = client.get(f"{API_URL}/{endpoint}")
+    response = client.get(f"{API_BASE_URL}/{endpoint}")
     response.raise_for_status()
     return response.json()
 
 
 def get_summary() -> Summary:
-    summary = get_api_data('team/summary')
+    summary = get_api_data("team/summary")
     return Summary.from_dict(summary)
 
 
 def get_roster() -> Roster:
-    roster = get_api_data('team/roster')
+    roster = get_api_data("team/roster")
     return Roster.from_dict(roster)
 
 
 def get_matches() -> MatchesResponse:
-    matches = get_api_data('team/stats/matches')
+    matches = get_api_data("team/stats/matches")
     return MatchesResponse.from_dict(matches)
 
 
 def get_news() -> NewsResponse:
-    news = get_api_data('team/news')
+    news = get_api_data("team/news")
     return NewsResponse.from_dict(news)
 
 
