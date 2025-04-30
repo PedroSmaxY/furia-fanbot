78f53756691fc355929dd6cf46f735495219174a
PedroSmaxY <pedrohnovais@outlook.com>
2025-04-30 15:11:45 -0300

    corrijindo linguagem da pagina para melhor seo

--- landing-page/src/app/layout.tsx
+++ landing-page/src/app/layout.tsx
@@ -80,7 +80,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
       >
